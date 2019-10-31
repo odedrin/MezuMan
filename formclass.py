6c6ad26d0d3463a5ad48d92ba8ec6cd825d8740e
Odedrin <odedrin@gmail.com>
2019-10-31 19:11:24 +0200

    Old files deleted

--- formclass.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-class newuser(Form):
-    name = TextField('Name:', validators=[DataRequired(), Length(min=2, max=20)])
-
-class newgroup(Form):
-    name = TextField('Name:', validators=[DataRequired(), Length(min=2, max=20)])
